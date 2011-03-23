431fe889364005ff3ad53f00a5ddc82645c0ac59
nov matake <nov@matake.jp>
2011-03-24 02:05:45 +0900

    fix fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-def setup_token(request, token)
+def setup_response(response, access_token)
   response.access_token = access_token.token
   response.token_type = access_token.token_type
   response.expires_in = access_token.expires_in
@@ -11,16 +11,16 @@ token_endpoint = Rack::OAuth2::Server::Token.new do |req, res|
   when :authorization_code
     code = AuthorizationCode.valid.find_by_code(req.code)
     req.invalid_grant! if code.blank? || code.redirect_uri != req.redirect_uri
-    setup_token res, code.access_token
+    setup_response res, code.access_token
   when :password
     # NOTE: password is not hashed in this sample app. Don't do the same on your app.
     account = Account.find_by_username_and_password(req.username, req.password) || req.invalid_grant!
-    setup_token req, account.access_tokens.create(:client => client)
+    setup_response res, account.access_tokens.create(:client => client)
   when :client_credentials
     # NOTE: client is already authenticated here.
-    setup_token req, client.access_tokens.create
+    setup_response res, client.access_tokens.create
   else
-    # NOTE: shouldn't be called since rack-oauth2 doesn't support any other grant_types (SAML assertion etc.) for now.
+    # NOTE: refresh_token and other assertions are not supported yet.
     req.unsupported_grant_type!
   end
 end
