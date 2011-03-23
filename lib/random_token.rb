8cee74e7ff15f3d742773d9d717fab30658b2f50
nov matake <nov@matake.jp>
2011-03-24 01:49:59 +0900

    random token generator update

--- lib/random_token.rb
+++ lib/random_token.rb
@@ -1,5 +1,5 @@
 module RandomToken
-  def self.generate(length = 64)
-    Base64.encode64(OpenSSL::Random.random_bytes(length)).gsub(/\n/, '')
+  def self.generate(bytes = 64)
+    ActiveSupport::SecureRandom.base64(bytes)
   end
 end
